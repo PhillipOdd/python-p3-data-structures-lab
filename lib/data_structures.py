939fbdead7cbfa7929c1dabd0fa382fec3303170
PhillipOdd <135106680+PhillipOdd@users.noreply.github.com>
2023-09-06 09:13:48 +0300

    Add files via upload

---
+++ lib/data_structures.py
@@ -0,0 +1,60 @@
+#1
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    },
+]
+
+def get_names(spicy_foods):
+    return [food["name"] for food in spicy_foods]
+
+
+def get_spiciest_foods(spicy_foods):
+    return[food for food in spicy_foods if food["heat_level"] > 5]
+
+def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+
+def print_spiciest_foods(spicy_foods):
+    food_spiciest = get_spiciest_foods(spicy_foods)
+    for food in food_spiciest:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+   
+
+def get_average_heat_level(spicy_foods):
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    if num_foods == 0:
+        return 0
+    average = total_heat_level/num_foods
+    return int(average)
+
+
+
+def create_spicy_food(spicy_foods, spicy_food):
+    new_spicy_foods = spicy_foods.copy()
+    new_spicy_foods.append(spicy_food)
+    return new_spicy_foods
+
